c92b4c838a2b1942afcc8f7f4409ca06dd3c7f59
Ruhaim Izmeth <ruhaim@gmail.com>
2018-03-05 18:16:44 +0530

    added to load from proper table

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const pg = require('pg');
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const cool = require("cool-ascii-faces");
+const express = require("express");
+const pg = require("pg");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 app = express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req,res)=> res.send(cool()))
-  .get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
-})
-  .get('/db', function (request, response) {
-	  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result) {
-		  done();
-		  if (err)
-		   { console.error(err); response.send("Error " + err); }
-		  else
-		   { response.render('pages/db', {results: result.rows} ); }
-		});
-	  });
-	})
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/cool", (req, res) => res.send(cool()))
+  .get("/times", function(request, response) {
+    var result = "";
+    var times = process.env.TIMES || 5;
+    for (i = 0; i < times; i++) result += i + " ";
+    response.send(result);
+  })
+  .get("/db", function(request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query("SELECT * FROM public.books", function(err, result) {
+        done();
+        if (err) {
+          console.error(err);
+          response.send("Error " + err);
+        } else {
+          response.render("pages/db", { results: result.rows });
+        }
+      });
+    });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-  //SELECT * FROM public.books
-  
-  //INSERT INTO public.books(
-	//"bookID", "bookName", "bookAuthor", "bookYear", "bookPrice")
-	//VALUES (?, ?, ?, ?, ?);
-	
+//SELECT * FROM public.books
+
+//INSERT INTO public.books(
+//"bookID", "bookName", "bookAuthor", "bookYear", "bookPrice")
+//VALUES (?, ?, ?, ?, ?);
+
 //Host
 //ec2-54-243-239-66.compute-1.amazonaws.com
 //Database
@@ -50,4 +51,3 @@ app = express()
 //postgres://lodoogsrewuqnk:f4bfa87e2c13e2ff246cd28baf9e8e828b5bf55763240756d217b099ecba510a@ec2-54-243-239-66.compute-1.amazonaws.com:5432/d7ob7gfv5p3kcq
 //Heroku CLI
 //heroku pg:psql postgresql-round-12981 --app sheltered-gorge-85466
-	
