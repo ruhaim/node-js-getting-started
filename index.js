4e7210bc39f03a475266a49d0d7630617e7eabe9
Ruhaim Izmeth <ruhaim@gmail.com>
2018-03-06 09:08:31 +0530

    added cors and remove unused routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const pg = require("pg");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const uuidv4 = require("uuid/v4");
 
 const { Pool, Client } = require("pg");
@@ -16,45 +17,10 @@ const pool = new Pool({
 app = express()
   .use(express.static(path.join(__dirname, "public")))
   .use(bodyParser.json())
+  .use(cors())
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
-  .get("/cool", (req, res) => res.send(cool()))
-  .get("/times", function(request, response) {
-    var result = "";
-    var times = process.env.TIMES || 5;
-    for (i = 0; i < times; i++) result += i + " ";
-    response.send(result);
-  })
-  .get("/db", function(request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query("SELECT * FROM public.books", function(err, result) {
-        done();
-        if (err) {
-          console.error(err);
-          response.send("Error " + err);
-        } else {
-          response.render("pages/db", { results: result.rows });
-        }
-      });
-    });
-  })
-
-  .get("/api/get_booksp", function(request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query("SELECT * FROM public.books", function(err, result) {
-        done();
-        if (err) {
-          console.error(err);
-          response.send("Error " + err);
-        } else {
-          //response.render("pages/db", { results: result.rows });
-          response.setHeader("Content-Type", "application/json");
-          response.send(JSON.stringify({ result: result.rows }));
-        }
-      });
-    });
-  })
   .get("/api/get_books", function(request, response) {
     pool.connect().then(client => {
       return client
@@ -138,23 +104,3 @@ app = express()
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-//SELECT * FROM public.books
-
-//INSERT INTO public.books(
-//"bookID", "bookName", "bookAuthor", "bookYear", "bookPrice")
-//VALUES (?, ?, ?, ?, ?);
-
-//Host
-//ec2-54-243-239-66.compute-1.amazonaws.com
-//Database
-//d7ob7gfv5p3kcq
-//User
-//lodoogsrewuqnk
-//Port
-//5432
-//Password
-//f4bfa87e2c13e2ff246cd28baf9e8e828b5bf55763240756d217b099ecba510a
-//URI
-//postgres://lodoogsrewuqnk:f4bfa87e2c13e2ff246cd28baf9e8e828b5bf55763240756d217b099ecba510a@ec2-54-243-239-66.compute-1.amazonaws.com:5432/d7ob7gfv5p3kcq
-//Heroku CLI
-//heroku pg:psql postgresql-round-12981 --app sheltered-gorge-85466
